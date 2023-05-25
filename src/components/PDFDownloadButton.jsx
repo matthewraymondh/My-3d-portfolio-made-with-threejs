be1c02f40c0076d578e3dee5f63082c4ded223ee
matthewraymondh <raymondhartono76@gmail.com>
2023-05-25 22:22:14 +0700

    fix cv download1

--- src/components/PDFDownloadButton.jsx
+++ src/components/PDFDownloadButton.jsx
@@ -6,8 +6,8 @@ const PDFDownloadButton = () => {
     // This could be a server request to generate the PDF or a direct link to a static PDF file
     // For the sake of this example, let's assume the PDF file already exists and we're linking to it
     const link = document.createElement("a");
-    link.href = "/src/assets/matthewraymondcv.pdf";
-    link.download = "matthewraymondcv.pdf";
+    link.href = "/src/assets/matthewraymondcv-5d7b6074.pdf";
+    link.download = "Matthew Raymond CV.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
